de5e4428021d16646955588889abaaba56e5cfa4
Nandhu <knandhu.27@gmail.com>
2020-01-20 14:03:16 -0800

    heroku fix5

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use("/api/tweets", tweets);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("frontend/public"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
   });
