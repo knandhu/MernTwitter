a216920e22064715ca74b772e12e8b2cc3c890c2
Nandhu <knandhu.27@gmail.com>
2020-01-20 13:59:04 -0800

    heroku deploy fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/api/tweets", tweets);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
   });
 }
 
